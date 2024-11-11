558416f36cc4a4f73d56ebda2eb5f216e6d368a6
Danat Ali Muradov <danatmuradov500@gmail.com>
2024-11-10 20:33:54 -0500

    Completed the Employee class. Added argument validate, getters, hashcode, equals, and toString.

--- FoodDeliverySystem/src/main/java/com/expressswallows/Employee.java
+++ FoodDeliverySystem/src/main/java/com/expressswallows/Employee.java
@@ -1,27 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.expressswallows;
 
-import java.util.Date;
+import com.expressswallows.utils.Utils;
+import java.time.LocalDate;
 
 /**
- *
+ * Employee class used to represent an employee.
  * @author shahi
  */
 public class Employee extends Person{
-    private static int count = 1;
+    private static int nextID = 1;
     private int employeeId;
 
-    public Employee(String firstName, String lastName, String email, String password, Date dob, String phoneNumber) {
+    /**
+     * All argument constructor for an Employee object.
+     * 
+     * @param firstName the first name of the employee.
+     * @param lastName the last name of the employee.
+     * @param email the email of the employee.
+     * @param password the password of the employee.
+     * @param dob the date of birth of the employee.
+     * @param phoneNumber the phone number of the employee.
+     */
+    public Employee(String firstName, String lastName, String email, String password, LocalDate dob, String phoneNumber) {
         super(firstName, lastName, email, password, dob, phoneNumber);
-        this.employeeId = count++;
+        // Validate the age of the employee
+        if (!Utils.validateEmployeeAge(dob)) {
+            throw new IllegalArgumentException("The employee must be at least 18 years of age.");
+        }
+        this.employeeId = nextID++;
     }
-    
-    public boolean validateAge(Date date){
-        return false;
+
+    /**
+     * Getter for the employee ID.
+     * @return the employee ID.
+     */
+    public int getEmployeeId() {
+        return employeeId;
     }
-    
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + this.employeeId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Employee other = (Employee) obj;
+        return this.employeeId == other.employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee{ID: %d, FirstName: %s, LastName: %s, "
+                + "Email: %s, Dob: %s, Phone: %s}", employeeId, getFirstName(), 
+                getLastName(), getEmail(), getDob(), getPhoneNumber());
+    }    
 }
