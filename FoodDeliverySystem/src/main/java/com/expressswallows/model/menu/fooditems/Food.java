cccc9522623abaff20826c0626783d4d2555c08d
Danat <danatmuradov500@gmail.com>
2024-11-18 21:34:26 -0500

    (Task #7) Updated the  super class. Updated the getters, setters, constructors, etc, and added code documentation.

--- FoodDeliverySystem/src/main/java/com/expressswallows/model/menu/fooditems/Food.java
+++ FoodDeliverySystem/src/main/java/com/expressswallows/model/menu/fooditems/Food.java
@@ -1,50 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.expressswallows.model.menu.fooditems;
 
 /**
- *
+ * Food abstract class is used to represent food items sold by the restaurant.
  * @author shahi
  */
 public abstract class Food {
-    protected int cookTime;
-    protected int productId;
-    protected double price;
+    private int productId;
+    private int cookTime;
+    private double price;
 
-    public Food(int cookTime, int productId, double price) {
+    /**
+     * All argument constructor for a food item.
+     * 
+     * @param cookTime the time it takes to cook the food item in minutes.
+     * @param price the price of the food item.
+     */
+    public Food(int cookTime, double price) {
         this.cookTime = cookTime;
-        this.productId = productId;
         this.price = price;
     }
 
+    /**
+     * Getter for the cook time.
+     * @return the cook time.
+     */
     public int getCookTime() {
         return cookTime;
     }
 
-    public void setCookTime(int cookTime) {
-        this.cookTime = cookTime;
-    }
-
+    /**
+     * Getter for the product ID.
+     * @return the product ID.
+     */
     public int getProductId() {
         return productId;
     }
 
+    /**
+     * Setter for the product ID.
+     * Used since the ID is assigned when the food item is inserted into the
+     * database and not when the food object is created.
+     * @param productId the product ID.
+     */
     public void setProductId(int productId) {
         this.productId = productId;
     }
 
+    /**
+     * Getter for the food item's price.
+     * @return the price of the food item.
+     */
     public double getPrice() {
         return price;
     }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
     
-    
-    
-    public abstract Food prepare(Food food);
-    
+    /**
+     * Abstract method prepare is used by subclasses to let them initialize
+     * their fields with their own predefined values.
+     * 
+     * @return the food item (the object itself) initialized with the right
+     * values.
+     */
+    public abstract Food prepare();
 }
