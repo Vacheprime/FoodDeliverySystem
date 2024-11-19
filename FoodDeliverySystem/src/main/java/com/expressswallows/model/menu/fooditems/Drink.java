464f85fc5c075cb1a8f33e77747aba87b60b6686
Danat <danatmuradov500@gmail.com>
2024-11-18 22:54:04 -0500

    Refactored the  parent class. Added code documentation and completed the class methods.

--- FoodDeliverySystem/src/main/java/com/expressswallows/model/menu/fooditems/Drink.java
+++ FoodDeliverySystem/src/main/java/com/expressswallows/model/menu/fooditems/Drink.java
@@ -1,5 +1,7 @@
 package com.expressswallows.model.menu.fooditems;
 
+import java.util.Objects;
+
 /**
  * Drink class is used to represent 
  * @author Andrew and Danat
@@ -17,12 +19,80 @@ public abstract class Drink extends Food {
         LARGE
     }
 
-    public Drink(int cookTime, int productId, double price, Size size) {
+    /**
+     * All argument constructor for a drink object.
+     * 
+     * @param cookTime the cook time of the drink.
+     * @param price the price of the drink.
+     * 
+     */
+    public Drink(int cookTime, double price) {
         super(cookTime, price);
+    }
+
+    /**
+     * Getter for the size of the drink.
+     * @return the size of the drink.
+     */
+    public Size getSize() {
+        return size;
+    }
+
+    /**
+     * Setter for the size of the drink.
+     * @param size the new size of the drink.
+     */
+    public void setSize(Size size) {
+        if (size == null) {
+            throw new IllegalArgumentException("The size cannot be null.");
+        }
         this.size = size;
     }
 
-    public Size getSize() {
-        return size;
+    /**
+     * Getter for the sugar content of the drink.
+     * @return the sugar content of the drink.
+     */
+    public int getSugarContent() {
+        return sugarContent;
+    }
+
+    /**
+     * Setter for the sugar content of the drink.
+     * @param sugarContent the new sugar content of the drink.
+     */
+    public void setSugarContent(int sugarContent) {
+        this.sugarContent = sugarContent;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + Objects.hashCode(this.size);
+        hash = 71 * hash + this.sugarContent;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Drink other = (Drink) obj;
+        if (this.sugarContent != other.sugarContent) {
+            return false;
+        }
+        return this.size == other.size;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" + "size=" + size + ", sugarContent=" + sugarContent + '}';
     }
 }
