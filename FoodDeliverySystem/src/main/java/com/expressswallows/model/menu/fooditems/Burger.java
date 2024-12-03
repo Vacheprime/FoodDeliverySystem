e02e8a258564989cbb7ca203a88d07c5cd4944be
Danat <danatmuradov500@gmail.com>
2024-12-02 23:09:49 -0500

    (Task #7) Developed the Burger abstract class.

--- FoodDeliverySystem/src/main/java/com/expressswallows/model/menu/fooditems/Burger.java
+++ FoodDeliverySystem/src/main/java/com/expressswallows/model/menu/fooditems/Burger.java
@@ -4,14 +4,71 @@
  */
 package com.expressswallows.model.menu.fooditems;
 
+import java.util.Objects;
+
 /**
- *
+ * Burger class is used to represent all food items that are burgers.
  * @author shahi
  */
 public abstract class Burger extends Food{
+    private BurgerSauce sauce;
 
-    public Burger(int cookTime, int productId, double price) {
-        super(cookTime, price);
+    /**
+     * BurgerSauce enum is used to represent all possible burger
+     * sauces.
+     */
+    public enum BurgerSauce {
+        SOGGY,
+        WHITE
     }
-    
+
+    /**
+     * All argument constructor for a new burger.
+     * @param sauce the sauce type.
+     */
+    public Burger(BurgerSauce sauce) {
+        super();
+        if (sauce == null) {
+            throw new IllegalArgumentException("The sauce argument cannot be null.");
+        }
+        this.sauce = sauce;
+    }
+
+    /**
+     * Getter for the burger sauce.
+     * @return the sauce type.
+     */
+    public BurgerSauce getSauce() {
+        return sauce;
+    }
+
+    /**
+     * Setter for the burger sauce type.
+     * @param sauce
+     */
+    public void setSauce(BurgerSauce sauce) {
+        if (sauce == null) {
+            throw new IllegalArgumentException("The sauce argument cannot be null.");
+        }
+        this.sauce = sauce;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Burger burger)) return false;
+        if (!super.equals(o)) return false;
+        return sauce == burger.sauce;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), sauce);
+    }
+
+    @Override
+    public String toString() {
+        return "Burger{" +
+                "sauce=" + sauce +
+                '}';
+    }
 }
